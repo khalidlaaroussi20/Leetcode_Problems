c49318d773dccde26f5b3c5d0dd68eb73ed3d7da
KhalidLaaroussi <49157461+khalidlaaroussi20@users.noreply.github.com>
2022-10-11 21:37:23 +0100

    Time: 570 ms (5.09%), Space: 61.6 MB (24.07%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) 
+    {
+        int cnt = 1;
+        int tab[3]= {-1,-1,-1};
+        tab[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+             if (nums[i] > tab[cnt - 1])
+            {
+                tab[cnt] = nums[i];
+                cnt++;
+            }
+            else
+            {
+                 if (cnt == 1)
+                    tab[0] = min(tab[0], nums[i]);
+                else if (cnt == 2)
+                {
+                    if (nums[i] > tab[0])
+                        tab[1] = nums[i];
+                    else if (nums[i] < tab[0])
+                        tab[0] = nums[i];
+                }
+            }
+            //printf ("count = %d ===== ",cnt);
+            for (int i = 0; i < 3; i++)
+                //printf("%d ", tab[i]);
+            printf("\n");
+            if (cnt == 3)
+                return (true);
+        }
+        return (false);
+    }
+};
